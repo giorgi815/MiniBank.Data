b229a8b7fc910ff7510ad38ed06cd3aec0f1b6c1
GIO <giochkhartishvili6@gmail.com>
2026-03-22 15:30:18 +0400

    operations added

--- Minibank.Repository/OperationRepository.cs
+++ Minibank.Repository/OperationRepository.cs
@@ -1,6 +1,7 @@
 ﻿using AccountRepository.Models;
 using AccountRepository.Models.Enums;
 using AccountRepository.Repositories;
+using System.Xml.Linq;
 
 
 namespace Minibank.Repository
@@ -9,7 +10,7 @@ namespace Minibank.Repository
     {
         private AccountRepository.Repositories.AccountRepository _accountRepository;
         private readonly List<Operation> _operation;
-        private string filePath = @"../../../../Operation.xml";
+        private string filePath = @"C:\Users\gioch\source\repos\MiniBank.Data\Account.Data\Operation.xml";
 
 
 
@@ -18,6 +19,7 @@ namespace Minibank.Repository
         public OperationRepository()
         {
             _accountRepository = new AccountRepository.Repositories.AccountRepository();
+            _operation = LoadFromXml();
         }
 
         public int Transfer(int fromAccountId, int toAccountId, decimal amount)
@@ -32,25 +34,33 @@ namespace Minibank.Repository
             if (fromAccount.Balance < amount)
                 throw new Exception("Inssuficient balance");
 
+            var operations = LoadFromXml();
+
+            int nextId = operations.Any() ? operations.Max(x => x.Id) + 1 : 1;
 
 
-            //Operation from = new Operation()
-            //{
-            //    Id = _operation.Any() ? _operation.Max(x => x.Id) + 1 : 1,
-            //    AcountId = fromAccountId,
-            //    Amount = -amount,
-            //    HappendAt = DateTime.Now,
-            //    operationType = OperationType.debit
-            //};
+            Operation from = new Operation()
+            {
+                Id = nextId,
+                AcountId = fromAccountId,
+                Amount = -amount,
+                HappendAt = DateTime.Now,
+                operationType = OperationType.debit
+            };
 
-            //Operation to = new Operation()
-            //{
-            //    Id = _operation.Any() ? _operation.Max(x => x.Id) + 1 : 1,
-            //    AcountId = fromAccountId,
-            //    Amount = amount,
-            //    HappendAt = DateTime.Now,
-            //    operationType = OperationType.debit
-            //};
+            Operation to = new Operation()
+            {
+                Id = nextId,
+                AcountId = toAccountId,
+                Amount = amount,
+                HappendAt = DateTime.Now,
+                operationType = OperationType.credit
+            };
+
+            operations.Add(from);
+            operations.Add(to);
+
+            SaveToXml(operations);
 
             var updateSender = fromAccount.Balance -= amount;
             var updateReciver = toAccount.Balance += amount;
@@ -76,6 +86,22 @@ namespace Minibank.Repository
 
             _accountRepository.UpdateAccount(fromAccount);
 
+            var operations = LoadFromXml();
+
+            int nextId = operations.Any() ? operations.Max(x => x.Id) + 1 : 1;
+
+            Operation operation = new Operation()
+            {
+                Id = nextId,
+                AcountId = accountId,
+                Amount = -amount,
+                HappendAt = DateTime.Now,
+                operationType = OperationType.debit
+            };
+
+            operations.Add(operation);
+            SaveToXml(operations);
+
             return 1;
 
         }
@@ -91,8 +117,67 @@ namespace Minibank.Repository
 
             _accountRepository.UpdateAccount(fromAccount);
 
+            var operations = LoadFromXml();
+
+            int nextId = operations.Any() ? operations.Max(x => x.Id) + 1 : 1;
+
+            Operation operation = new Operation()
+            {
+                Id = nextId,
+                AcountId = accountId,
+                Amount = amount,
+                HappendAt = DateTime.Now,
+                operationType = OperationType.credit
+            };
+
+            operations.Add(operation);
+            SaveToXml(operations);
+
+
             return 1;
         }
+
+        #region Helpres
+
+        private List<Operation> LoadFromXml()
+        {
+            if (!File.Exists(filePath))
+                return new List<Operation>();
+
+            var doc = XDocument.Load(filePath);
+
+            return doc.Root.Elements("Operation")
+                .Select(x => new Operation
+                {
+                    Id = (int)x.Element("Id"),
+                    AcountId = (int)x.Element("AccountId"),
+                    Amount = (decimal)x.Element("Amount"),
+                    HappendAt = (DateTime)x.Element("HappenedAt"),
+                    operationType = Enum.Parse<OperationType>(x.Element("Type").Value)
+                }).ToList();
+        }
+
+        private void SaveToXml(List<Operation> operations)
+        {
+            var doc = new XDocument(
+                new XElement("Operations",
+                    operations.Select(op =>
+                        new XElement("Operation",
+                            new XElement("Id", op.Id),
+                            new XElement("AccountId", op.AcountId),
+                            new XElement("Amount", op.Amount),
+                            new XElement("HappenedAt", op.HappendAt),
+                            new XElement("Type", op.operationType.ToString())
+                        )
+                    )
+                )
+            );
+
+            doc.Save(filePath);
+        }
+
+
+        #endregion
 
     }
 }
